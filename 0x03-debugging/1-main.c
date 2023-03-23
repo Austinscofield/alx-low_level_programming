0640105c5fbfd8a264e328de2c6d004a460c6dbf
austinscofield <ukatu7@gmail.com>
2023-03-23 01:03:30 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n")
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 	/**
@@ -22,6 +22,4 @@ int main(void)
 
 	return (0);
 }
-
-
 
